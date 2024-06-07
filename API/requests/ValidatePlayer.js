eaa66d1543c95a4f51e0381482cc2864a16ce278
cetteup <me@cetteup.com>
2024-06-07 17:27:25 +0200

    fix: Provide ValidatePlayer response as expected by game server

--- API/requests/ValidatePlayer.js
+++ API/requests/ValidatePlayer.js
@@ -1,16 +1,29 @@
 module.exports = function(req, res, next) {
-    var header = {"asof":req.currentTime,"pid":req.profileid || null};
+    var header = {
+        "pid": null, // actual pid from session
+        "nick": null, // actual nick from session
+        "spid": req.queryParams.pid, // pid as passed in request
+        "asof": req.currentTime
+    };
+
     var result;
     if(req.session_valid) {
+        header["pid"] = req.profileid;
+        header["nick"] = req.profile.uniquenick;
         if(req.profile.uniquenick != req.queryParams.SoldierNick) {
             result = "InvalidReportedNick";
         } else if(req.profileid != req.queryParams.pid) {
             result = "InvalidReportedProfileID";
         } else {
             result = "Ok";
-            header["nick"] = req.profile.uniquenick;
         }
     } else {
+        // `pid` and/or `nick` need to differ from request in order to cause a kick/ban
+        header["pid"] = 0;
+        // `[prefix] nick` usually get cut off after 23 characters in the game's client-server protocols
+        // While the limit appears to not be applied to values returned by the validation,
+        // it's probably best to follow that convention/limit
+        header["nick"] = ("INVALID " + req.queryParams.SoldierNick).slice(0, 23)
         result = "InvalidAuthProfileID";
     }
             
