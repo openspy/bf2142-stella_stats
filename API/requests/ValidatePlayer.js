9a75c0fed86de9e6f11184a21c1ae95ff9d55a81
cetteup <me@cetteup.com>
2024-06-07 17:04:44 +0200

    fix: Return distinct error codes when player validation fails

--- API/requests/ValidatePlayer.js
+++ API/requests/ValidatePlayer.js
@@ -1,12 +1,19 @@
 module.exports = function(req, res, next) {
     var header = {"asof":req.currentTime,"pid":req.profileid || null};
+    var result;
     if(req.session_valid) {
-        if(req.profile.uniquenick != req.queryParams.SoldierNick || req.profileid != req.queryParams.pid) {
-            req.session_valid = false;
+        if(req.profile.uniquenick != req.queryParams.SoldierNick) {
+            result = "InvalidReportedNick";
+        } else if(req.profileid != req.queryParams.pid) {
+            result = "InvalidReportedProfileID";
+        } else {
+            result = "Ok";
+            header["nick"] = req.profile.uniquenick;
         }
-        header["nick"] = req.profile.uniquenick;
+    } else {
+        result = "InvalidAuthProfileID";
     }
             
-    var send_entries = [[header], [{"result": req.session_valid ? "OK" : "NAK"}]];
+    var send_entries = [[header], [{"result": result}]];
     req.sendResponse(res, send_entries);
 };
